4afb6f0362c7f7435d855c70e9da0e9f2f3cbfff
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-05-30 01:00:01 +0600

    404 not found page has been done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className='not-found'>
+                <h1> 
+                    <span>404</span> 
+                    Opps Page Not Found
+                </h1>
+                <button>
+                    <Link to='/'>Back to Home Page</Link> 
+                </button>
+                
         </div>
     );
 };
