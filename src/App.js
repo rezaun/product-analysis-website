86d607cd487cad1aec4f7c0aead07b8f0348f42e
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-05-29 18:05:54 +0600

    Navbar Done with active menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews'
+import Blog from './Components/Blog/Blog'
+import Navbar from './Components/Navbar/Navbar';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/reviews' element={<Reviews/>}/>
+        <Route path='/blogs' element={<Blog/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../Assets/Images/logo.png'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+       <nav>
+           <div className="logo">
+               <img src={Logo} alt="" />
+           </div>
+           <div className="menu">
+               <NavLink className={({ isActive }) => (isActive ? "active-menu" : "link")} to='/'>Home</NavLink>
+               <NavLink className={({ isActive }) => (isActive ? "active-menu" : 'link')} to='/about'>About</NavLink>
+               <NavLink className={({ isActive }) => (isActive ? "active-menu" : 'link')} to='/reviews'>Reviews</NavLink>
+               <NavLink className={({ isActive }) => (isActive ? "active-menu" : 'link')} to='/blogs'>Blog</NavLink>
+           </div>
+       </nav>
+    );
+};
+
+export default Navbar;
