063bc61865716864447bc0082dab3d4f36631fab
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-06-02 20:08:49 +0600

    about page content added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About Page</h2>
+        <>
+        <h2>Welcome To burger review</h2>
+        <div className='about-us'>
+            <div className="about-us-left">
+            Burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+            burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+            burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+
+            <p>I will keep posting more important posts on my Website for all of you. Please give your support and love.</p>
+
+            
+
+            
+            </div>
+            <div className="about-us-right">
+            Burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+            burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+            burger review is a Professional review Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of review, 
+            with a focus on dependability and review. We're working to turn our passion for review into a booming online website. We hope you enjoy our review as much as we enjoy offering them 
+            to you.
+
+            <p>I will keep posting more important posts on my Website for all of you. Please give your support and love.</p>
+
+            
+
+            
+            </div>
+           
+           
         </div>
+        </>
     );
 };
 
