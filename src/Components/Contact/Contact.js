4b96b01d6951b90a88e25794fc2c215efbb84832
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-06-03 03:10:33 +0600

    contact page reload solved delete form tag

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,36 +4,30 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div className='contact'>
-            <h1>Contact With Us</h1>
-            <form >          
-               
-                <table>
-                    <tr>
-                        <th>Name: </th>
-                        <td><input type="text"  placeholder='Your Name'/></td>
-                    </tr>
-                    <tr>
-                        <th>Email: </th>
-                        <td><input type="email"  placeholder='Your Email'/></td>
-                    </tr>
-                    <tr>
-                        <th>Message: </th>
-                        <td><textarea placeholder='Your Message'></textarea></td>
-                    </tr> 
-                    <tr>
-                        <th>
-                           
-                        </th>
-                        <td>
-                             <button className='submitBtn' type='submit'>Submit</button>
-                        </td>
-                         
-                    </tr>
-                   
-                </table>
-                
-            </form>
-           
+            <h1>Contact With Us</h1>             
+            <table>
+                <tr>
+                    <th>Name: </th>
+                    <td><input type="text"  placeholder='Your Name'/></td>
+                </tr>
+                <tr>
+                    <th>Email: </th>
+                    <td><input type="email"  placeholder='Your Email'/></td>
+                </tr>
+                <tr>
+                    <th>Message: </th>
+                    <td><textarea placeholder='Your Message'></textarea></td>
+                </tr> 
+                <tr>
+                    <th>
+                        
+                    </th>
+                    <td>
+                            <button className='submitBtn' type='submit'>Submit</button>
+                    </td>                         
+                </tr>                   
+            </table>
+        
         </div>
     );
 };
